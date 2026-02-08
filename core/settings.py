ca9dc7da9fd0b6d290e2de9c1478fb1a7d6b1f70
tinajoelly <tinaandrea.joelly2@edu.fh-kaernten.ac.at>
2026-02-08 12:47:31 +0100

    set cors allowed origins as env variables

--- core/settings.py
+++ core/settings.py
@@ -52,10 +52,7 @@ INSTALLED_APPS = [
     'videoflix_app',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-]
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", default="http://localhost:5500").split(",")
 
 CORS_ALLOW_CREDENTIALS = True
 
